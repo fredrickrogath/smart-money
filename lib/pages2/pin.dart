9856b018e31e5971cff3aeec560018075f77b2aa
fredrick rogath <fredlee560@gmail.com>
2022-04-22 16:47:59 +0300

    smartmoney

--- lib/pages2/pin.dart
+++ lib/pages2/pin.dart
@@ -1,15 +1,23 @@
-import 'package:animations/animations.dart';
+// import 'package:animations/animations.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:smartmoney/pages2/budget.dart';
-import 'package:smartmoney/pages2/new-budget.dart';
-import 'package:smartmoney/pages2/welcome.dart';
+// import 'package:smartmoney/pages2/new-budget.dart';
+// import 'package:smartmoney/pages2/welcome.dart';
+import 'package:http/http.dart' as http;
 
 class Pin extends StatefulWidget {
   final String name;
   final String phone;
   final String countryCode;
-  const Pin({Key? key, required this.name, required this.phone, required this.countryCode}) : super(key: key);
+  const Pin(
+      {Key? key,
+      required this.name,
+      required this.phone,
+      required this.countryCode})
+      : super(key: key);
 
   @override
   State<Pin> createState() => _PinState();
@@ -18,6 +26,69 @@ class Pin extends StatefulWidget {
 class _PinState extends State<Pin> {
   double frameHeight = 0;
   double frameWidth = 0;
+  String accessToken = '';
+
+//   Future<http.Response> createAlbum(String title) {
+//     return http.post(
+//       Uri.parse('http://127.0.0.1:8000/login'),
+//       headers: <String, String>{
+//         'Content-Type': 'application/json; charset=UTF-8',
+//       },
+//       body: jsonEncode(<String, String>{
+//         'mobile': '+255715983180',
+//         'password': '1'
+//       }),
+//   );
+
+//   if (response.statusCode == 201) {
+//     // If the server did return a 201 CREATED response,
+//     // then parse the JSON.
+//     return Album.fromJson(jsonDecode(response.body));
+//   } else {
+//     // If the server did not return a 201 CREATED response,
+//     // then throw an exception.
+//     throw Exception('Failed to create album.');
+//   }
+// }
+
+  // Future login() async {
+  //   final response = await http.post(
+  //     Uri.parse('http://127.0.0.1:8080/api/login'),
+  //     headers: <String, String>{
+  //       'Content-Type': 'application/json; charset=UTF-8',
+  //     },
+  //     body: jsonEncode(
+  //         <String, String>{'mobile': '+255715983180', 'password': '1'}),
+  //   );
+
+  //   if (response.statusCode == 201) {
+  //     // If the server did return a 201 CREATED response,
+  //     // then parse the JSON.
+  //     print(jsonDecode(response.body));
+  //     // return jsonDecode(response.body);
+  //   } else {
+  //     // If the server did not return a 201 CREATED response,
+  //     // then throw an exception.
+  //     throw Exception('Failed to create album.');
+  //   }
+  // }
+
+  void login() async {
+    // This example uses the Google Books API to search for books about http.
+    // https://developers.google.com/books/docs/overview
+    var url = Uri.http('10.0.2.2:8000', '/api/login',
+        {'mobile': '+255715983180', 'password': '1'});
+
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.post(url);
+    if (response.statusCode == 200) {
+      var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
+      // var itemCount = jsonResponse['totalItems'];
+      print(jsonResponse);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -95,6 +166,7 @@ class _PinState extends State<Pin> {
                   //     child: Text('Submit',style: TextStyle(fontSize:18.0),),
                   //   ),
                   // ),
+
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 10.0),
                     child: SizedBox(
@@ -103,14 +175,15 @@ class _PinState extends State<Pin> {
                       child: FloatingActionButton.extended(
                         elevation: 0.0,
                         onPressed: () {
-                          Navigator.push(
-                              context,
-                              PageTransition(
-                                  duration: const Duration(milliseconds: 700),
-                                  reverseDuration:
-                                      const Duration(milliseconds: 700),
-                                  type: PageTransitionType.rightToLeftWithFade,
-                                  child: const Budget()));
+                          login();
+                          // Navigator.push(
+                          //     context,
+                          //     PageTransition(
+                          //         duration: const Duration(milliseconds: 700),
+                          //         reverseDuration:
+                          //             const Duration(milliseconds: 700),
+                          //         type: PageTransitionType.rightToLeftWithFade,
+                          //         child: const Budget()));
                         },
                         label: const Text('Save'),
                         // icon: const Icon(Icons.remove),
