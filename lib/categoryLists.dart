faa66de8a8c0296eec4839151b0fcf3fead0fea7
fredrick rogath <fredlee560@gmail.com>
2022-05-03 11:10:22 +0300

    sm

---
+++ lib/categoryLists.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CategoryLists extends StatefulWidget {
+  CategoryLists({Key? key}) : super(key: key);
+
+  @override
+  State<CategoryLists> createState() => _CategoryListsState();
+}
+
+class _CategoryListsState extends State<CategoryLists> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
--- lib/pages2/in_entry.dart
+++ lib/pages2/in_entry.dart
@@ -25,10 +25,10 @@ class _InEntryState extends State<InEntry> {
 
   final _formKey = GlobalKey<FormState>();
 
-  final List<String> genderItems = [
-    'Ujasi',
-    'Msalato',
-  ];
+  // List<String> categoryLIst = [
+  //   'Ujasi',
+  //   'Msalato',
+  // ];
 
   String? selectedValue;
 
@@ -52,7 +52,10 @@ class _InEntryState extends State<InEntry> {
     // Await the http get response, then decode the json-formatted response.
     var response = await http.post(url, headers: requestHeaders);
     if (response.statusCode == 200) {
-      categories = jsonDecode(response.body)['data'];
+      // categories = jsonDecode(response.body)['data'];
+      categories = json.decode(response.body);
+      // categories.cast<String>();
+      // categoryLIst = Map<String, dynamic>.from(json.decode(response.body))['data'].cast<String>();
       print(categories);
     } else {
       print('Request failed with status: ${response.statusCode}.');
@@ -73,6 +76,8 @@ class _InEntryState extends State<InEntry> {
   @override
   void initState() {
     // TODO: implement initState
+    print(categories);
+    // print('2222222' + categoryLIst.toString());
     refresh();
     super.initState();
   }
@@ -129,58 +134,105 @@ class _InEntryState extends State<InEntry> {
 
               SizedBox(height: MediaQuery.of(context).size.height / 30),
 
-              DropdownButtonFormField2(
-                decoration: InputDecoration(
-                  //Add isDense true and zero Padding.
-                  //Add Horizontal padding using buttonPadding and Vertical padding by increasing buttonHeight instead of add Padding here so that The whole TextField Button become clickable, and also the dropdown menu open under The whole TextField Button.
-                  isDense: true,
-                  contentPadding: EdgeInsets.zero,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
+              // DropdownButtonFormField2(
+              //   decoration: InputDecoration(
+              //     //Add isDense true and zero Padding.
+              //     //Add Horizontal padding using buttonPadding and Vertical padding by increasing buttonHeight instead of add Padding here so that The whole TextField Button become clickable, and also the dropdown menu open under The whole TextField Button.
+              //     isDense: true,
+              //     contentPadding: EdgeInsets.zero,
+              //     border: OutlineInputBorder(
+              //       borderRadius: BorderRadius.circular(15),
+              //     ),
+              //     //Add more decoration as you want here
+              //     //Add label If you want but add hint outside the decoration to be aligned in the button perfectly.
+              //   ),
+              //   isExpanded: true,
+              //   hint: const Text(
+              //     'Category',
+              //     style: TextStyle(fontSize: 14),
+              //   ),
+              //   icon: const Icon(
+              //     Icons.arrow_drop_down,
+              //     color: Colors.black45,
+              //   ),
+              //   iconSize: 30,
+              //   buttonHeight: 60,
+              //   buttonPadding: const EdgeInsets.only(left: 20, right: 10),
+              //   dropdownDecoration: BoxDecoration(
+              //     borderRadius: BorderRadius.circular(15),
+              //   ),
+              //   items: categories
+                
+              //       .map((item) => DropdownMenuItem<String>(
+              //             value: item,
+                          
+              //             child: Text(
+              //               item.to,
+              //               style: const TextStyle(
+              //                 fontSize: 14,
+              //               ),
+              //             ),
+              //           ))
+              //       .toList(),
+              //   // validator: (value) {
+              //   //   if (value == null) {
+              //   //     return 'Please select gender.';
+              //   //   }
+              //   // },
+              //   onChanged: (value) {
+              //     //Do something when changing the item if you want.
+              //   },
+              //   onSaved: (value) {
+              //     selectedValue = value.toString();
+              //   },
+              // ),
+
+
+              // DropdownButton(
+              //                   isExpanded: true,
+              //                   value: categories,
+              //                   hint: Text("Select "),
+              //                   items: countries.map((countryone){
+              //                     return DropdownMenuItem(
+              //                       child: Text(countryone), //label of item
+              //                       value: countryone, //value of item
+              //                     );
+              //                    }).toList(), 
+              //                    onChanged: (value){
+              //                        countryname = value; //change the country name
+              //                        getCity(); //get city list.
+              //                    },
+              //               ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: SizedBox(
+                  height: frameHeight / 17.0,
+                  width: double.infinity,
+                  child: FloatingActionButton.extended(
+                    elevation: 0.0,
+                    onPressed: () {
+                      setState(() {});
+                      if (_controller.value.text.isNotEmpty) {
+                        // Navigator.push(
+                        //     context,
+                        //     PageTransition(
+                        //         duration: const Duration(milliseconds: 700),
+                        //         reverseDuration:
+                        //             const Duration(milliseconds: 700),
+                        //         type:
+                        //             PageTransitionType.rightToLeftWithFade,
+                        //         child: Welcome(name:_controller.text)));
+                      }
+                    },
+                    label: Text('Select category', style: TextStyle(color: Colors.grey[700]),),
+                    // icon: const Icon(Icons.remove),
+                    backgroundColor: Colors.white,
                   ),
-                  //Add more decoration as you want here
-                  //Add label If you want but add hint outside the decoration to be aligned in the button perfectly.
                 ),
-                isExpanded: true,
-                hint: const Text(
-                  'Category',
-                  style: TextStyle(fontSize: 14),
-                ),
-                icon: const Icon(
-                  Icons.arrow_drop_down,
-                  color: Colors.black45,
-                ),
-                iconSize: 30,
-                buttonHeight: 60,
-                buttonPadding: const EdgeInsets.only(left: 20, right: 10),
-                dropdownDecoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                items: genderItems
-                    .map((item) => DropdownMenuItem<String>(
-                          value: item,
-                          child: Text(
-                            item,
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                        ))
-                    .toList(),
-                // validator: (value) {
-                //   if (value == null) {
-                //     return 'Please select gender.';
-                //   }
-                // },
-                onChanged: (value) {
-                  //Do something when changing the item if you want.
-                },
-                onSaved: (value) {
-                  selectedValue = value.toString();
-                },
               ),
 
-              SizedBox(height: MediaQuery.of(context).size.height / 20),
+              SizedBox(height: MediaQuery.of(context).size.height / 30),
 
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 10.0),
